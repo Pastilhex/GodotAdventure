c0d6b970d05f27e838033019986db0e03510e55f
Pastilhex <ivodanielmarques@gmail.com>
2023-06-03 16:25:22 +0100

    first commit

---
+++ Characters/Scripts/mask_dude.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+var jump_count = 0
+var jump_speed = -250
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func horizontal_animation(direction):
+	#Se a direção for diferente de 0 move-se para os lados
+	if direction != 0:
+		velocity.x = direction * SPEED
+		$Animation.play("run")
+		if direction > 0:
+			get_node("Texture").flip_h = false
+		else:
+			get_node("Texture").flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$Animation.play("idle")
+
+func vertical_animation():
+	#Se a velocidade vertical for diferente de 0 é pq está no ar
+	if velocity.y < 0:
+		$Animation.play("jump")
+	elif velocity.y > 0:
+		$Animation.play("fall")
+
+func jump():
+	if is_on_floor():
+		jump_count = 0
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		velocity.y = jump_speed
+		jump_count += 1
+		print(jump_count)
+	
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("left", "right")
+	horizontal_animation(direction)
+	vertical_animation()
+	jump()
+	move_and_slide()
+	
+
