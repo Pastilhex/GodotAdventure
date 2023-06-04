dbfd26de48e6a7bfc07a730430ab80a19747c7f9
Pastilhex <ivodanielmarques@gmail.com>
2023-06-04 10:16:59 +0100

    Animated Dead Bat

--- Characters/Scripts/bat.gd
+++ Characters/Scripts/bat.gd
@@ -14,8 +14,11 @@ func kill():
 	if state != States.DEAD:
 		state = States.DEAD
 		$AnimationPlayer.play("hit")
-	if is_on_floor():
-		queue_free()
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position - Vector2(0, -30), 0.25)
+		var tween1 = get_tree().create_tween()
+		tween1.tween_property(self, "modulate:a", 0, 0.25)
+		tween.tween_callback(queue_free)
 
 func alive() -> bool:
 	if state != States.DEAD:
@@ -37,5 +40,5 @@ func _physics_process(delta):
 	elif state == States.DEAD:
 		velocity.x = move_toward(velocity.x, 0, (speed * delta) / 0.8)
 		velocity.y += gravity * delta
-		queue_free()
+		#queue_free()
 	move_and_slide()
