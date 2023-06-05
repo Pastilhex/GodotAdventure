6df1192d2bc3dfeaec80773ec4b792c3060aa975
Pastilhex <ivodanielmarques@gmail.com>
2023-06-05 19:00:54 +0100

    Tree House almost ready

--- Houses/tree_house.gd
+++ Houses/tree_house.gd
@@ -6,13 +6,12 @@ var readyToOpen = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Globals.inside_buildings = true
-	pass
 
 
 func _process(delta):
 	if Input.is_action_pressed("enter") && readyToOpen == true:
 		get_tree().change_scene_to_file(Globals.stage)
-
+	$Candle.play("candle")
 
 func _on_tree_house_door_body_entered(body):
 	if body.name == "MaskDude":
